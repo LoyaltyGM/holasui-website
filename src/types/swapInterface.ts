5a8ed140c84a4fb407effa878646b48835dcee4d
hokage229 <ernestzulpukarov@gmail.com>
2023-06-14 01:16:12 +0600

    change IOffer interface

--- src/types/swapInterface.ts
+++ src/types/swapInterface.ts
@@ -1,50 +1,45 @@
-import {Dispatch, SetStateAction} from "react";
+import { Dispatch, SetStateAction } from "react";
 
 export type TradeObjectType = {
-    id: string;
-    url: string;
+  id: string;
+  url: string;
 };
 
 export type TabType = "sent" | "received";
 
-export interface IDetailOfferProps {
-  offer_id: IOffer;
-}
-
-
 export interface ISwapInformation {
-    userObjectIds: TradeObjectType[];
-    setShowCollection: any;
-    setCoinAmount: any;
-    coinAmount: number | null;
-    recipientAddress?: string;
-    isRecipient?: boolean;
+  userObjectIds: TradeObjectType[];
+  setShowCollection: any;
+  setCoinAmount: any;
+  coinAmount: number | null;
+  recipientAddress?: string;
+  isRecipient?: boolean;
 }
 
 export interface ISwapCollectionDialog {
-    wallet: any;
-    opened: boolean;
-    setOpened: Dispatch<SetStateAction<boolean>>;
-    batchIdTrade: TradeObjectType[];
-    setBatchIdTrade: Dispatch<SetStateAction<TradeObjectType[]>>;
+  wallet: any;
+  opened: boolean;
+  setOpened: Dispatch<SetStateAction<boolean>>;
+  batchIdTrade: TradeObjectType[];
+  setBatchIdTrade: Dispatch<SetStateAction<TradeObjectType[]>>;
 }
 
 export interface ISwapRecipientCollectionDialog extends ISwapCollectionDialog {
-    walletAddressToSearch: string | undefined;
-    setWalletAddressToSearch: Dispatch<SetStateAction<string | undefined>>;
+  walletAddressToSearch: string | undefined;
+  setWalletAddressToSearch: Dispatch<SetStateAction<string | undefined>>;
 }
 
 export interface IOffer {
-    id: string;
-    active: boolean;
-    object_bag: any;
-    status: number;
-    //
-    creator: string;
-    creator_items: any;
-    creator_coin: number;
-    //
-    recipient: string;
-    recipient_items_ids: any;
-    recipient_coin_amount: number;
+  id: string;
+  status: number;
+  escrowed_items: any;
+  escrowed_coin: any;
+  //
+  creator: string;
+  creator_items_ids: any;
+  creator_coin_amount: number;
+  //
+  recipient: string;
+  recipient_items_ids: any;
+  recipient_coin_amount: number;
 }
