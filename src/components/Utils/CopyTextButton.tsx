9cf85c828a5e58eaed10967b660b5dfdff909ff5
hokage229 <ernestzulpukarov@gmail.com>
2023-06-14 02:24:47 +0600

    change CopyTextButton.tsx component

--- src/components/Utils/CopyTextButton.tsx
+++ src/components/Utils/CopyTextButton.tsx
@@ -2,7 +2,7 @@ import { CheckIcon, ClipboardDocumentIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import { useBoolean, useCopyToClipboard } from "usehooks-ts";
 
-export const CopyTextButton = ({ copyText }: { copyText: string }) => {
+export const CopyTextButton = ({ showText, copyText }: { showText: string; copyText: string }) => {
   const { value: isCopied, setTrue, setFalse } = useBoolean();
   const [, copy] = useCopyToClipboard();
 
@@ -16,8 +16,8 @@ export const CopyTextButton = ({ copyText }: { copyText: string }) => {
   };
 
   return (
-    <div className={"flex gap-2 items-center hover:text-base-content/50 hover:cursor-pointer"} onClick={handleClick}>
-      {copyText}
+    <div className={"flex items-center hover:text-base-content/50 hover:cursor-pointer"} onClick={handleClick}>
+      {showText}
       {isCopied ? <CheckIcon className="h-6" /> : <ClipboardDocumentIcon className="h-6" />}
     </div>
   );
