0e1be9d4feea223dd8cad8be2f4f56093485f15a
Vakurin Maxim <Vakurin.maxim@gmail.com>
2023-06-27 19:51:57 -0300

    update ticker animation

--- src/components/Layout/NewsTicker.tsx
+++ src/components/Layout/NewsTicker.tsx
@@ -1,35 +1,29 @@
 export const NewsTicker = () => {
   // TODO: need to fix animation 33% percent
   const dotsClasses = "text-[3rem] mb-1 leading-1";
+  const textClasses = "mx-4 text-4xl";
   return (
-    <div className="mb-1 hidden overflow-x-hidden bg-yellowColor text-white md:flex">
-      <div className="flex w-full animate-marquee space-x-4 py-1 font-bold">
-        <div className="flex content-center items-center justify-center space-x-4 whitespace-nowrap">
-          <span className="text-3xl">SUI X KUCOIN HACKATON</span>
-          <span className={dotsClasses}>·</span>
-          <span className="text-3xl">JOIN OUR COMMUNITY</span>
-          <span className={dotsClasses}>·</span>
-          <span className="text-3xl">ATTRIBUTES DAO!</span>
-          <span className={dotsClasses}>·</span>
-        </div>
+    <div className="relative flex overflow-x-hidden bg-yellowColor font-bold text-white">
+      <div className="animate-marquee whitespace-nowrap py-1 uppercase">
+        <span className="mx-4 text-4xl">SUI X KUCOIN HACK</span>
+        <span className={dotsClasses}>·</span>
+        <span className="mx-4 text-4xl">ATTRIBUTES DAO</span>
+        <span className={dotsClasses}>·</span>
+        <span className="mx-4 text-4xl">HOLA EVERYONE!</span>
+        <span className={dotsClasses}>·</span>
+        <span className="mx-4 text-4xl">DAO CONSTRUCTOR</span>
+        <span className={dotsClasses}>·</span>
+      </div>
 
-        <div className="flex content-center items-center justify-center space-x-4 whitespace-nowrap">
-          <span className="text-3xl">SUI X KUCOIN HACKATON</span>
-          <span className={dotsClasses}>·</span>
-          <span className="text-3xl">JOIN OUR COMMUNITY</span>
-          <span className={dotsClasses}>·</span>
-          <span className="text-3xl">ATTRIBUTES DAO!</span>
-          <span className={dotsClasses}>·</span>
-        </div>
-
-        <div className="flex content-center items-center justify-center space-x-4 whitespace-nowrap">
-          <span className="text-3xl">SUI X KUCOIN HACKATON</span>
-          <span className={dotsClasses}>·</span>
-          <span className="text-3xl">JOIN OUR COMMUNITY</span>
-          <span className={dotsClasses}>·</span>
-          <span className="text-3xl">ATTRIBUTES DAO!</span>
-          <span className={dotsClasses}>·</span>
-        </div>
+      <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-1 uppercase">
+        <span className="mx-4 text-4xl">SUI X KUCOIN HACK</span>
+        <span className={dotsClasses}>·</span>
+        <span className="mx-4 text-4xl">ATTRIBUTES DAO</span>
+        <span className={dotsClasses}>·</span>
+        <span className="mx-4 text-4xl">HOLA EVERYONE!</span>
+        <span className={dotsClasses}>·</span>
+        <span className="mx-4 text-4xl">DAO CONSTRUCTOR</span>
+        <span className={dotsClasses}>·</span>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,12 +36,17 @@ module.exports = {
         black3Color: `rgb((89,89,89) / 60)`,
       },
       animation: {
-        marquee: "marquee 32s linear infinite",
+        marquee: "marquee 20s linear infinite",
+        marquee2: "marquee2 20s linear infinite",
       },
       keyframes: {
         marquee: {
           "0%": { transform: "translateX(0%)" },
           "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
         },
       },
     },
