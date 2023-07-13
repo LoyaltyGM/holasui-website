2abb4104d6ed0949855c191a309dc3a918b7b57a
hokage229 <ernestzulpukarov@gmail.com>
2023-07-13 23:19:05 +0600

    update constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -30,7 +30,7 @@ export const PRICE_ESCROW: number = 0.1;
 export const PACKAGE_ID_V2 =
   "0x4164dcc89e0b20d9b30754596e9118381cf8ccf03155528f9f43da21c81195dd";
 export const ORIGIN_CAPY_DAO_ID =
-  "0x19ef9b5e5581182568f44934fc4dc4d03b04cded6ff120e78357ff7da53675ca";
+  "0xa4b0851e9c356afe86945bd36f15f0fb7d91b819078613047572c3b419ab3a0c";
 export const DAO_HUB_ID = "0x72caf5bcb54146520e4c5a435edf1d76d09e319157c31ced9154bbc2ea2fa313";
 
 // ==== IPFS ====
